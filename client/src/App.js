38731f76e40f40d0a09afa32bdef919a492de8c4
Conji <dubstepandponies95@gmail.com>
2020-06-27 03:23:08 -0400

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledApp = styled.div`
+  margin: 0;
+  padding: 8px;
+  color: white;
+`;
+
+const App = () => <StyledApp>Hello world</StyledApp>;
+
+export default App;
